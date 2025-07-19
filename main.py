9d8ffed8959970b6ca043bcc1b1737088b6e7222
Akash Patel <113753334+Akashpatel2609@users.noreply.github.com>
2025-07-18 23:07:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app_mode = st.sidebar.selectbox("Select Page",["Home","About","Disease Recogniti
 
 #Home Page
 if(app_mode=="Home"):
-    st.header("PLANT DISEASE RECOGNITION SYSTEM")
+    st.header("AGRI-VISION: PLANT DISEASE RECOGNITION SYSTEM")
     image_path = "home_page.jpeg"
     st.image(image_path, use_container_width=True)  # Fixed deprecation warning
     st.markdown("""
